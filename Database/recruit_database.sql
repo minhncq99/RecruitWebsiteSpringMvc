f60015db0cf66edcb4d1855be6ec5eb96c684d35
Minh <minhncq99@gmail.com>
2021-04-14 14:06:00 +0700

    change database

--- Database/recruit_database.sql
+++ Database/recruit_database.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: javadb
+-- Host: 127.0.0.1    Database: javatest
 -- ------------------------------------------------------
 -- Server version	8.0.23
 
@@ -16,60 +16,64 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `applican`
+-- Table structure for table `applicant`
 --
 
-DROP TABLE IF EXISTS `applican`;
+DROP TABLE IF EXISTS `applicant`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `applican` (
-  `id` int NOT NULL,
+CREATE TABLE `applicant` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `gender` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `experiance` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `career_id` int DEFAULT NULL,
+  `experiance` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `career_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `applican_career_idx` (`career_id`),
-  CONSTRAINT `applican_career` FOREIGN KEY (`career_id`) REFERENCES `career` (`id`),
-  CONSTRAINT `applican_user` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  KEY `applicant_career_idx` (`career_id`),
+  CONSTRAINT `applicant_career` FOREIGN KEY (`career_id`) REFERENCES `career` (`id`),
+  CONSTRAINT `applicant_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `applican`
+-- Dumping data for table `applicant`
 --
 
-LOCK TABLES `applican` WRITE;
-/*!40000 ALTER TABLE `applican` DISABLE KEYS */;
-/*!40000 ALTER TABLE `applican` ENABLE KEYS */;
+LOCK TABLES `applicant` WRITE;
+/*!40000 ALTER TABLE `applicant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `applicant` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `applican_news`
+-- Table structure for table `applicant_news`
 --
 
-DROP TABLE IF EXISTS `applican_news`;
+DROP TABLE IF EXISTS `applicant_news`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `applican_news` (
-  `applican_id` int NOT NULL,
+CREATE TABLE `applicant_news` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `applicant_id` int NOT NULL,
   `news_id` int NOT NULL,
   `date` datetime NOT NULL,
   `state` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`applican_id`,`news_id`),
-  KEY `applican_news_newsid_idx` (`news_id`),
-  CONSTRAINT `applican_news_applicanid` FOREIGN KEY (`applican_id`) REFERENCES `applican` (`id`),
-  CONSTRAINT `applican_news_newsid` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`)
+  PRIMARY KEY (`id`),
+  KEY `applicant_news_newsid_idx` (`news_id`),
+  KEY `applicant_news_applicantid_idx` (`applicant_id`),
+  CONSTRAINT `applicant_news_applicantid` FOREIGN KEY (`applicant_id`) REFERENCES `applicant` (`id`),
+  CONSTRAINT `applicant_news_newsid` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `applican_news`
+-- Dumping data for table `applicant_news`
 --
 
-LOCK TABLES `applican_news` WRITE;
-/*!40000 ALTER TABLE `applican_news` DISABLE KEYS */;
-/*!40000 ALTER TABLE `applican_news` ENABLE KEYS */;
+LOCK TABLES `applicant_news` WRITE;
+/*!40000 ALTER TABLE `applicant_news` DISABLE KEYS */;
+/*!40000 ALTER TABLE `applicant_news` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -81,8 +85,9 @@ DROP TABLE IF EXISTS `career`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `career` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -92,7 +97,7 @@ CREATE TABLE `career` (
 
 LOCK TABLES `career` WRITE;
 /*!40000 ALTER TABLE `career` DISABLE KEYS */;
-INSERT INTO `career` VALUES (1,'An toàn lao động'),(2,'Bán hàng kỹ thuật'),(3,'Bán lẻ / bán sỉ'),(4,'Báo chí / Truyền hình'),(5,'Bảo hiểm'),(6,'Bảo trì / Sữa chữa'),(7,'Bất động sản'),(8,'Biên / Phiên dịch'),(9,'Bưu chính - Viễn thông'),(10,'Chứng khoán / Vàng / Ngoại tệ'),(11,'Cơ khí / Chế tạo / Tự động hóa'),(12,'Công nghệ cao'),(13,'Công nghệ Ô tô'),(14,'Công nghệ thông tin'),(15,'Dầu khí/Hóa chất'),(16,'Dệt may / Da giày'),(17,'Địa chất / Khoáng sản'),(18,'Dịch vụ khách hàng'),(19,'Điện / Điện tử / Điện lạnh'),(20,'Điện tử viễn thông'),(21,'Du lịch'),(22,'Dược phẩm / Công nghệ sinh học'),(23,'Giáo dục / Đào tạo'),(24,'Hàng cao cấp'),(25,'Hàng gia dụng'),(26,'Hàng hải'),(27,'Hàng không'),(28,'Hàng tiêu dùng'),(29,'Hành chính / Văn phòng'),(30,'Hoá học / Sinh học'),(31,'Hoạch định/Dự án'),(32,'In ấn / Xuất bản'),(33,'IT Phần cứng / Mạng'),(34,'IT phần mềm'),(35,'Kế toán / Kiểm toán'),(36,'Khách sạn / Nhà hàng'),(37,'Kiến trúc'),(38,'Kinh doanh / Bán hàng'),(39,'Logistics'),(40,'Luật/Pháp lý'),(41,'Marketing / Truyền thông / Quảng cáo'),(42,'Môi trường / Xử lý chất thải'),(43,'Mỹ phẩm / Trang sức'),(44,'Mỹ thuật / Nghệ thuật / Điện ảnh'),(45,'Ngân hàng / Tài chính'),(46,'Ngành nghề khác'),(47,'NGO / Phi chính phủ / Phi lợi nhuận'),(48,'Nhân sự'),(49,'Nông / Lâm / Ngư nghiệp'),(50,'Phi chính phủ / Phi lợi nhuận'),(51,'Quản lý chất lượng (QA/QC)'),(52,'Quản lý điều hành'),(53,'Sản phẩm công nghiệp'),(54,'Sản xuất'),(56,'Spa / Làm đẹp'),(57,'Tài chính / Đầu tư'),(58,'Thiết kế đồ họa'),(59,'Thiết kế nội thất'),(60,'Thời trang'),(61,'Thư ký / Trợ lý'),(62,'Thực phẩm / Đồ uống'),(63,'Tổ chức sự kiện / Quà tặng'),(64,'Tư vấn'),(65,'Vận tải / Kho vận'),(66,'Xây dựng'),(67,'Xuất nhập khẩu'),(68,'Y tế / Dược');
+INSERT INTO `career` VALUES (1,'An toàn lao động'),(2,'Bán hàng kỹ thuật'),(3,'Bán lẻ / bán sỉ'),(4,'Báo chí / Truyền hình'),(5,'Bảo hiểm'),(6,'Bảo trì / Sữa chữa'),(7,'Bất động sản'),(8,'Biên / Phiên dịch'),(9,'Bưu chính - Viễn thông'),(10,'Chứng khoán / Vàng / Ngoại tệ'),(11,'Cơ khí / Chế tạo / Tự động hóa'),(12,'Công nghệ cao'),(13,'Công nghệ Ô tô'),(14,'Công nghệ thông tin'),(15,'Dầu khí/Hóa chất'),(16,'Dệt may / Da giày'),(18,'Dịch vụ khách hàng'),(21,'Du lịch'),(22,'Dược phẩm / Công nghệ sinh học'),(17,'Địa chất / Khoáng sản'),(19,'Điện / Điện tử / Điện lạnh'),(20,'Điện tử viễn thông'),(23,'Giáo dục / Đào tạo'),(24,'Hàng cao cấp'),(25,'Hàng gia dụng'),(26,'Hàng hải'),(27,'Hàng không'),(28,'Hàng tiêu dùng'),(29,'Hành chính / Văn phòng'),(30,'Hoá học / Sinh học'),(31,'Hoạch định/Dự án'),(32,'In ấn / Xuất bản'),(33,'IT Phần cứng / Mạng'),(34,'IT phần mềm'),(35,'Kế toán / Kiểm toán'),(36,'Khách sạn / Nhà hàng'),(37,'Kiến trúc'),(38,'Kinh doanh / Bán hàng'),(39,'Logistics'),(40,'Luật/Pháp lý'),(41,'Marketing / Truyền thông / Quảng cáo'),(42,'Môi trường / Xử lý chất thải'),(43,'Mỹ phẩm / Trang sức'),(44,'Mỹ thuật / Nghệ thuật / Điện ảnh'),(45,'Ngân hàng / Tài chính'),(46,'Ngành nghề khác'),(47,'NGO / Phi chính phủ / Phi lợi nhuận'),(48,'Nhân sự'),(49,'Nông / Lâm / Ngư nghiệp'),(50,'Phi chính phủ / Phi lợi nhuận'),(51,'Quản lý chất lượng (QA/QC)'),(52,'Quản lý điều hành'),(53,'Sản phẩm công nghiệp'),(54,'Sản xuất'),(56,'Spa / Làm đẹp'),(57,'Tài chính / Đầu tư'),(58,'Thiết kế đồ họa'),(59,'Thiết kế nội thất'),(60,'Thời trang'),(61,'Thư ký / Trợ lý'),(62,'Thực phẩm / Đồ uống'),(63,'Tổ chức sự kiện / Quà tặng'),(64,'Tư vấn'),(65,'Vận tải / Kho vận'),(66,'Xây dựng'),(67,'Xuất nhập khẩu'),(68,'Y tế / Dược');
 /*!40000 ALTER TABLE `career` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,13 +109,15 @@ DROP TABLE IF EXISTS `employer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employer` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_id` int NOT NULL,
   `location_id` int NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
   KEY `employer_location_idx` (`location_id`),
   CONSTRAINT `employer_location` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
-  CONSTRAINT `employer_user` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
+  CONSTRAINT `employer_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -132,8 +139,9 @@ DROP TABLE IF EXISTS `location`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `location` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`,`name`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -143,7 +151,7 @@ CREATE TABLE `location` (
 
 LOCK TABLES `location` WRITE;
 /*!40000 ALTER TABLE `location` DISABLE KEYS */;
-INSERT INTO `location` VALUES (1,'Hà Nội'),(2,'Hồ Chí Minh'),(3,'Bình Dương'),(4,'Bắc Ninh'),(5,'Đồng Nai'),(6,'Hưng Yên'),(7,'Hải Dương'),(8,'Đà Nẵng'),(9,'Hải Phòng'),(10,'An Giang'),(11,'Bà Rịa-Vũng Tàu'),(12,'Bắc Giang'),(13,'Bạc Liêu'),(14,'Bến Tre'),(15,'Bình Định'),(16,'Bình Phước'),(17,'Bình Thuận'),(18,'Cà Mau'),(19,'Cần Thơ'),(20,'Cao Bằng'),(21,'Cửu Long'),(22,'Đắc Lắc'),(23,'Đắc Nông'),(24,'Điện Biên'),(25,'Đồng Tháp'),(26,'Gia Lai'),(27,'Hà Giang'),(28,'Hà Nam'),(29,'Hà Tĩnh'),(30,'Hậu Giang'),(31,'Hoà Bình'),(32,'Khánh Hoà'),(33,'Kiên Giang'),(34,'Kon Tum'),(35,'Lai Châu'),(36,'Lâm Đồng'),(37,'Lạng Sơn'),(38,'Lào Cai'),(39,'Long An'),(40,'Nam Định'),(41,'Nghệ An'),(42,'Ninh Bình'),(43,'Ninh Thuận'),(44,'Phú Thọ'),(45,'Phú Yên'),(46,'Quảng Bình'),(47,'Quảng Nam'),(48,'Quảng Ngãi'),(49,'Quảng Ninh'),(50,'Quảng Trị'),(51,'Sóc Trăng'),(52,'Sơn La'),(53,'Tây Ninh'),(54,'Thái Bình'),(55,'Thái Nguyên'),(56,'Thanh Hoá'),(57,'Thừa Thiên Huế'),(58,'Tiền Giang'),(59,'Trà Vinh'),(60,'Tuyên Quang'),(61,'Vĩnh Long'),(62,'Vĩnh Phúc'),(63,'Yên Bái'),(64,'Nước Ngoài');
+INSERT INTO `location` VALUES (10,'An Giang'),(11,'Bà Rịa-Vũng Tàu'),(12,'Bắc Giang'),(13,'Bạc Liêu'),(4,'Bắc Ninh'),(14,'Bến Tre'),(3,'Bình Dương'),(15,'Bình Định'),(16,'Bình Phước'),(17,'Bình Thuận'),(18,'Cà Mau'),(19,'Cần Thơ'),(20,'Cao Bằng'),(21,'Cửu Long'),(8,'Đà Nẵng'),(22,'Đắc Lắc'),(23,'Đắc Nông'),(24,'Điện Biên'),(5,'Đồng Nai'),(25,'Đồng Tháp'),(26,'Gia Lai'),(27,'Hà Giang'),(28,'Hà Nam'),(1,'Hà Nội'),(29,'Hà Tĩnh'),(7,'Hải Dương'),(9,'Hải Phòng'),(30,'Hậu Giang'),(2,'Hồ Chí Minh'),(31,'Hoà Bình'),(6,'Hưng Yên'),(32,'Khánh Hoà'),(33,'Kiên Giang'),(34,'Kon Tum'),(35,'Lai Châu'),(36,'Lâm Đồng'),(37,'Lạng Sơn'),(38,'Lào Cai'),(39,'Long An'),(40,'Nam Định'),(41,'Nghệ An'),(42,'Ninh Bình'),(43,'Ninh Thuận'),(64,'Nước Ngoài'),(44,'Phú Thọ'),(45,'Phú Yên'),(46,'Quảng Bình'),(47,'Quảng Nam'),(48,'Quảng Ngãi'),(49,'Quảng Ninh'),(50,'Quảng Trị'),(51,'Sóc Trăng'),(52,'Sơn La'),(53,'Tây Ninh'),(54,'Thái Bình'),(55,'Thái Nguyên'),(56,'Thanh Hoá'),(57,'Thừa Thiên Huế'),(58,'Tiền Giang'),(59,'Trà Vinh'),(60,'Tuyên Quang'),(61,'Vĩnh Long'),(62,'Vĩnh Phúc'),(63,'Yên Bái');
 /*!40000 ALTER TABLE `location` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -156,16 +164,16 @@ DROP TABLE IF EXISTS `news`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `news` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `salary` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `experience` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `type_working` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `location_id` int NOT NULL,
-  `career_id` int NOT NULL,
-  `employer_id` int NOT NULL,
+  `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `salary` int DEFAULT NULL,
+  `experiance` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type_working` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8_unicode_ci,
   `time_start` date NOT NULL,
   `time_end` date NOT NULL,
-  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `employer_id` int NOT NULL,
+  `career_id` int NOT NULL,
+  `location_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `news_location_idx` (`location_id`),
   KEY `news_career_idx` (`career_id`),
@@ -195,7 +203,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `role` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
@@ -223,4 +231,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-13 14:44:39
+-- Dump completed on 2021-04-13 20:20:14
