182f55b66eb5d3ff713316cb670eea6024c6170f
Minh <minhncq99@gmail.com>
2021-04-16 18:06:48 +0700

    Create News Persitant class

--- SpringWebMvc/src/main/java/com/java/controller/EmployerController.java
+++ SpringWebMvc/src/main/java/com/java/controller/EmployerController.java
@@ -1,14 +1,18 @@
 package com.java.controller;
 
+import com.java.pojo.ApplicantRegisterForm;
 import com.java.service.CareerService;
 import com.java.service.LocationService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -28,6 +32,7 @@ public class EmployerController {
     @ModelAttribute
     private void addModelAttribute(Model model) {
         model.addAttribute("cssfile", "employer");
+        model.addAttribute("news", "");
     }
 
     @RequestMapping({"/", "/{action}"})
@@ -42,6 +47,13 @@ public class EmployerController {
             model.addAttribute("location", this.locationService.getLocations());
             model.addAttribute("career", this.careerService.getCareers());
         }
+        return "employer";
+    }
+    
+    @PostMapping("/addNews")
+    public String createNews(Model model, 
+                @Valid @ModelAttribute(value = "applicant") ApplicantRegisterForm applicant,
+            BindingResult result) {
         return "employer";
     }
 
